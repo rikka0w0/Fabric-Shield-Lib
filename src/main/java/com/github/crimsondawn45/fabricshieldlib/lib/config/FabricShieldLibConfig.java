857add0ed0520188d8128e73665cc38752359407
Michael Arakelyan <gozzook@gmail.com>
2025-08-03 20:32:22 -0700

    setup config page and lang for further component tooltips

--- src/main/java/com/github/crimsondawn45/fabricshieldlib/lib/config/FabricShieldLibConfig.java
+++ src/main/java/com/github/crimsondawn45/fabricshieldlib/lib/config/FabricShieldLibConfig.java
@@ -3,21 +3,98 @@ package com.github.crimsondawn45.fabricshieldlib.lib.config;
 import eu.midnightdust.lib.config.MidnightConfig;
 
 public class FabricShieldLibConfig extends MidnightConfig {
-	@Entry
+
+	public static final String MAIN = "general";
+	public static final String TOOLTIP = "tooltip";
+
+
+	@Entry(category = MAIN)
 	public static boolean enable_tooltips = true;
 
-	@Comment
+	@Comment(category = MAIN)
 	public static Comment enchantability_convention;
 
-	@Entry
+	@Entry(category = MAIN)
 	public static int vanilla_shield_enchantability = 14;
 
-	@Entry
+	@Entry(category = MAIN)
 	public static boolean universal_disable = false;
 
-	@Comment
+	@Comment(category = MAIN)
 	public static Comment universal_disable_description_1;
 
-	@Comment
+	@Comment(category = MAIN)
 	public static Comment universal_disable_description_2;
+
+	@Entry(category = TOOLTIP)
+	public static boolean complex_tooltip = false;
+
+	@Condition(requiredOption = "fabricshieldlib:complex_tooltip", requiredValue = "true")
+	@Entry(category = TOOLTIP)
+	public static boolean block_delay_seconds = false;
+
+	@Comment(category = TOOLTIP) public static Comment spacer1;
+
+	@Condition(requiredOption = "fabricshieldlib:complex_tooltip", requiredValue = "true")
+	@Entry(category = TOOLTIP)
+	public static boolean cooldown_ticks = true;
+
+	@Comment(category = TOOLTIP) public static Comment spacer2;
+
+	@Condition(requiredOption = "fabricshieldlib:complex_tooltip", requiredValue = "true")
+	@Entry(category = TOOLTIP)
+	public static boolean damage_reduction_type = false;
+
+	@Comment(category = TOOLTIP) public static Comment spacer3;
+
+	@Condition(requiredOption = "fabricshieldlib:complex_tooltip", requiredValue = "true")
+	@Entry(category = TOOLTIP)
+	public static boolean damage_reduction_base = false;
+
+	@Comment(category = TOOLTIP) public static Comment spacer4;
+
+	@Condition(requiredOption = "fabricshieldlib:complex_tooltip", requiredValue = "true")
+	@Entry(category = TOOLTIP)
+	public static boolean damage_reduction_percent = false;
+
+	@Comment(category = TOOLTIP) public static Comment spacer5;
+
+	@Condition(requiredOption = "fabricshieldlib:complex_tooltip", requiredValue = "true")
+	@Entry(category = TOOLTIP)
+	public static boolean damage_reduction_blocking_angle = false;
+
+	@Comment(category = TOOLTIP) public static Comment spacer6;
+
+	@Condition(requiredOption = "fabricshieldlib:complex_tooltip", requiredValue = "true")
+	@Entry(category = TOOLTIP)
+	public static boolean item_damage_threshold = false;
+
+	@Comment(category = TOOLTIP) public static Comment spacer7;
+
+	@Condition(requiredOption = "fabricshieldlib:complex_tooltip", requiredValue = "true")
+	@Entry(category = TOOLTIP)
+	public static boolean item_damage_base = false;
+
+	@Comment(category = TOOLTIP) public static Comment spacer8;
+
+	@Condition(requiredOption = "fabricshieldlib:complex_tooltip", requiredValue = "true")
+	@Entry(category = TOOLTIP)
+	public static boolean item_damage_percent = false;
+
+	@Comment(category = TOOLTIP) public static Comment spacer9;
+
+	@Condition(requiredOption = "fabricshieldlib:complex_tooltip", requiredValue = "true")
+	@Entry(category = TOOLTIP)
+	public static boolean block_sound = false;
+
+	@Condition(requiredOption = "fabricshieldlib:complex_tooltip", requiredValue = "true")
+	@Entry(category = TOOLTIP)
+	public static boolean disabled_sound = false;
+
+	@Condition(requiredOption = "fabricshieldlib:complex_tooltip", requiredValue = "true")
+	@Entry(category = TOOLTIP)
+	public static boolean bypassed_by = false;
+
+
+
 }
